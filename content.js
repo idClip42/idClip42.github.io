c30546b54c11a9b0d02ea91f72e6ba3a02b1d545
Alexander Earley <axe6801@rit.edu>
2019-01-10 21:49:39 -0500

    Added Dropbox link to page
    Former-commit-id: 4d61f02127ec732475e52dfaf8bf0b368d603e91

--- content.js
+++ content.js
@@ -8,7 +8,8 @@ content = [
 		info: "My current, ongoing independent project.\nA murder mystery in which the player must determine which of 8 characters is killing off the others.",
 		controls: [],
 		links: {
-			"Play Demo (Firefox Strongly Recommended)" : "webgl/WhodunnitWebDemo1/index.html",
+			"Play Web Demo (Firefox Strongly Recommended)" : "webgl/WhodunnitWebDemo1/index.html",
+			"Download Latest Build" : "https://www.dropbox.com/sh/ci2ppw4b37sgshm/AACmfU4i5XGVEWjzg7YULJj4a?dl=0",
 			"Github Page" : "https://github.com/idClip42/Whodunnit",
 			"Development Blog" : "https://idclip42.tumblr.com/"
 		},
